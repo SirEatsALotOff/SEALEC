05f8d8640e3a2996b56dbdea9f05d2617c8b1a42
Sir Eats A Lot <sireatsalot@infinityminers.com>
2017-10-08 01:26:37 -0500

    Added Range Command

--- SECMD/Interface/SECMD.bat
+++ SECMD/Interface/SECMD.bat
@@ -8,10 +8,25 @@ echo Type +help to see additional utilities
 setlocal EnableDelayedExpansion
 set "maininputSEAL="
 set /p "maininputSEAL= %cd%>"
+set "SEALDETECT=%maininputSEAL:~0,1%"
+if "%SEALDETECT%"=="+" goto SealConsole
 
 %maininputSEAL%
-
 @echo off
 echo.
 endlocal
+goto start
+:SealConsole
+echo.
+set "SEALDETECT=%maininputSEAL:~0,6%"
+if "%SEALDETECT%"=="+range" goto range
+
+
+echo No Command Found
+goto start
+:range
+set "SEALDETECT2=%maininputSEAL:~7%"
+
+call C:\SECMD\JigglyBits\range.bat %SEALDETECT2%
+echo !SRangeC!
 goto start
