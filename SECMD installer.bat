2cf2d54713b139363c0b8036b56fbbf43f4e3ad7
Sir Eats A Lot <sireatsalot@infinityminers.com>
2017-10-08 00:57:36 -0500

    Updated Installer
    Fixed some bugs, and it now works in a very basic way. Simply because of the range.bat, I may make this a release after a few more additions.

--- SECMD installer.bat
+++ SECMD installer.bat
@@ -8,6 +8,7 @@ echo Also we're assuming you're running this file from the desktop.
 echo Reason is I like to store all my jiggly bits in your computers.
 echo.
 echo Are you sure about this?
+echo If you're updating, this will delete all files inside SECMD.
 echo We may or may not mess with random system files.
 echo Probs not, but what the heck
 echo Not responsible for stuff ya do... and uhm...
@@ -23,6 +24,9 @@ exit
 :install
 cls
 @echo on
+if exist C:\SECMD (
+RD /s /q C:\SECMD
+)
 @echo Creating Initial File...
 MKDIR C:\SECMD\LoveLife
 @echo Hitting ALT+F4...
@@ -33,29 +37,37 @@ MKDIR C:\SECMD\Resources\Sound
 MKDIR C:\SECMD\Resources\Shelf
 MKDIR C:\SECMD\Resources\ForDevelopers
 MKDIR C:\SECMD\Mods
+echo.>"C:\SECMD\Mods\ModList.txt"
 MKDIR C:\SECMD\Interface
 MKDIR C:\SECMD\JigglyBits
 @echo Righty then, that was the easy part. Hope this drive isn't a potato...
-call :downloadandcopy https://raw.githubusercontent.com/SirEatsALotOff/SEALEC/master/sealhelp.txt sealhelp.txt resources\Shelf
 @echo Lol, potato confirmed...
 @echo Huh, this download method would be a decent utility...
 @echo Welp, chugging on to the next download.
-call :downloadandcopy
+call :downloadandcopy https://raw.githubusercontent.com/SirEatsALotOff/SEALEC/master/SECMD/Interface/SECMD.bat SECMD.bat Interface
+call :downloadandcopy https://raw.githubusercontent.com/SirEatsALotOff/SEALEC/master/SECMD/JigglyBits/range.bat range.bat JigglyBits
+call :downloadandcopy https://raw.githubusercontent.com/SirEatsALotOff/SEALEC/master/SECMD/LoveLife/Version_News.txt Version_News.txt LoveLife
+call :downloadandcopy https://raw.githubusercontent.com/SirEatsALotOff/SEALEC/master/SECMD/Resources/Shelf/versioninfo.txt versioninfo.txt Resources\Shelf
+@echo Cleaning up mess...
+@RD /s /q C:\SECMD\Interface\SECMD
+@echo Literally I have no idea why it puts these things in there...
+@RD /s /q C:\SECMD\JigglyBits\SECMD
+@RD /s /q C:\SECMD\LoveLife\SECMD
+@echo Probably doubles download times tbh
+@echo OK that is weird, mod folder doesn't have it...
+@echo Moving on...
+@RD /s /q C:\SECMD\Resources\Shelf\SECMD
 ::%1 downoad link
 ::%2 Outfile
 ::%3 Copy Into
+echo.
 @echo DONE!
 pause
 exit
-:SpecificLine
-(
-echo hello
-) >variables.txt
-GOTO :EOF
 :downloadandcopy
 @echo DOWNLOADING CONTENT...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%1', '%2')"
-powershell -Command "Invoke-WebRequest %1 -OutFile %2.txt"
+powershell -Command "Invoke-WebRequest %1 -OutFile %2"
 xcopy /s "C:\Users\%username%\Desktop\%2" "C:\SECMD\%3"
 del C:\Users\%username%\Desktop\%2
 goto :EOF
